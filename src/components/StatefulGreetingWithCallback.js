4329ab8b6d1cc8270eac7a01ceb9856854f99dda
mr-fibonacci <adamlapinski1@gmail.com>
2021-02-26 14:29:14 +0000

    finished challenge

--- src/components/StatefulGreetingWithCallback.js
+++ src/components/StatefulGreetingWithCallback.js
@@ -12,8 +12,13 @@ class StatefulGreetingWithCallback extends React.Component {
   handleClick() {
     this.setState({
       introduction: "Goodbye!",
+      buttonText: 'Enter'
+    }, ()=>{
+        console.log('new state', this.state.introduction);
+        console.log('new state', this.state.buttonText);
     });
     console.log(this.state.introduction);
+    console.log(this.state.buttonText);
   }
 
   render() {
