a2350d8eba7621c34d74a3d7b1546ffc331d0869
mr-fibonacci <adamlapinski1@gmail.com>
2021-02-27 21:12:09 +0000

    new branch, video 018.4

--- src/components/StatefulGreetingWIthPrevState.js
+++ src/components/StatefulGreetingWIthPrevState.js
@@ -6,20 +6,36 @@ class StatefulGreetingWithPrevState extends React.Component {
     this.state = {
       introduction: "Hello!",
       buttonText: "Exit",
+      count: 0
     };
   }
 
   handleClick() {
     this.setState(
       {
-        introduction: "Goodbye!",
-        buttonText: "Enter",
+        introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+        buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
       },
       () => {
         console.log("new state", this.state.introduction);
         console.log("new state", this.state.buttonText);
       }
     );
+  }
+
+  increment() {
+      this.setState({
+          count: this.state.count + 1
+      })
+      console.log(this.state.count)
+  }
+
+  incrementFive(){
+      this.increment()
+      this.increment()
+      this.increment()
+      this.increment()
+      this.increment()
   }
 
   render() {
@@ -31,6 +47,10 @@ class StatefulGreetingWithPrevState extends React.Component {
         <button onClick={() => this.handleClick()}>
           {this.state.buttonText}
         </button>
+        <button onClick={() => this.incrementFive()}>
+          Increment five
+        </button>
+    <p>You've clicked {this.state.count} times</p>
       </div>
     );
   }
