22614d567d7669be1a127889f96a91ebd99446d8
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-05 22:15:17 +0000

    included components in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 
 function App() {
-  return <div className="App"></div>;
+  return (<div className="App">
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional />
+  </div>);
 }
 
 export default App;
