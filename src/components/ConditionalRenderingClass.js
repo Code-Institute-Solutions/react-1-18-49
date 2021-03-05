3be87c609837e693757ad8e41488ca9db3b80371
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-05 21:11:24 +0000

    new branch 021

---
+++ src/components/ConditionalRenderingClass.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ConditionalRenderingClass extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ConditionalRenderingClass
---
+++ src/components/ConditionalRenderingFunctional.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConditionalRenderingFunctional() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ConditionalRenderingFunctional
