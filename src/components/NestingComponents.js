98b36ebf061e93e33a2f8bf6580efc37da682705
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-06 16:30:57 +0000

    new branch 022

---
+++ src/components/NestingComponents.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class NestingComponents extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            isLoaded: true,
+            isLoggedIn: true,
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>{this.state.isLoaded ? 'Data loaded!' : 'Loading ...'}</h1>
+                {
+                    this.state.isLoggedIn ? (
+                        <div>
+                            <p>Welcome to the site! Please complete the steps below:</p>
+                            <ol>
+                                <li>Confirm your email</li>
+                                <li>Complete your profile</li>
+                                <li>Subscribe to the newsletter</li>
+                            </ol>
+                        </div>
+                    ) : (
+                        <p>Please sign in</p>
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+export default NestingComponents
