8f944f05ad37ac28a3d64af4908c6faba900d656
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-14 17:32:53 +0000

    finished challenge

--- src/components/LifeCyclesCDM.js
+++ src/components/LifeCyclesCDM.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import LifeCyclesCDMChild from './LifeCyclesCDMChild'
 
 export class LifeCyclesCDM extends Component {
+    constructor(props) {
+        console.log('constructor called')
+        super(props)
+    
+        this.state = {
+             data: 'loading'
+        }
+    }
+    getData() {
+        console.log('getData() called')
+        setTimeout(() => {
+            console.log('Data fetched!')
+            this.setState({
+                data: 'loaded'
+            })
+        }, 3000)
+    }
+    componentDidMount() {
+        console.log('Component mounted')
+        this.getData()
+    }
     render() {
+        console.log('Render method called')
         return (
             <div>
-                <h1>lifecycles</h1>
+                <h1>{this.state.data}</h1>
+                <LifeCyclesCDMChild />
             </div>
         )
     }
