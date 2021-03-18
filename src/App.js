218f50ed985c6ad33ef7c7e0e0d5aec293b76faf
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-18 09:25:57 +0000

    fixed main

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import UncontrolledForm from "./components/UncontrolledForm";
 
 function App() {
-  return <div className="App">
-      <UncontrolledForm />
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
