9821697e607eea83a76bfc874a870a0fd2f1c307
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-22 23:35:32 +0000

    finished challenge 041

--- src/components/UseEffectCounter.js
+++ src/components/UseEffectCounter.js
@@ -2,10 +2,18 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
         console.log('count 1 effect');
         document.title = count;
     }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
     return (
         <div>
             <button onClick={
@@ -13,6 +21,7 @@ function UseEffectCounter() {
             }>
                 Increment Count ({count})
             </button>
+            <h2>Time is {time}</h2>
         </div>
     )
 }
