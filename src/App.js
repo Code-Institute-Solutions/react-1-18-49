a10fe80b930175aaee43b39cd2adf590b34b8866
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-22 22:52:12 +0000

    new branch 040

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import ClassCounter from "./components/ClassCounter";
 import UseEffectCounter from "./components/UseEffectCounter";
 
 function App() {
   return <div className="App">
-      {/* <ClassCounter /> */}
       <UseEffectCounter />
   </div>;
 }
