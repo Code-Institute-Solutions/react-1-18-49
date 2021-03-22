68c2132a9ed2819525ae7f7bdbe7721e834f9fdb
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-22 23:22:20 +0000

    added console.log and [count] to useEffect

--- src/components/UseEffectCounter.js
+++ src/components/UseEffectCounter.js
@@ -3,8 +3,9 @@ import React, {useState, useEffect} from 'react'
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
     useEffect(() => {
+        console.log('count 1 effect');
         document.title = count;
-    });
+    }, [count]);
     return (
         <div>
             <button onClick={
