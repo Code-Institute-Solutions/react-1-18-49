37ed60da24bbe899e8dddcc2b8d5843f15560459
mr-fibonacci <adamlapinski1@gmail.com>
2021-03-31 23:10:06 +0000

    finished challenge 047

--- src/components/HTTPRequests.js
+++ src/components/HTTPRequests.js
@@ -6,23 +6,47 @@ export class HTTPRequests extends Component {
         super(props)
     
         this.state = {
-             posts: []
+             posts: [],
+             error: null
         }
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
         .then(response => {
             console.log(response);
             this.setState({
-                posts: response.data
+                posts: Array.isArray(response.data)
+                ? response.data
+                : [response.data]
+            })
+        })
+        .catch(error => {
+            this.setState({
+                error: error.message
             })
         })
     }
     render() {
+        const posts = this.state.posts;
         return (
             <div>
                 <h2>Posts:</h2>
-                {JSON.stringify(this.state.posts)}
+                {
+                    posts.length ? (
+                        posts.map(post => (
+                            <div key={post.id}>
+                                <h2>{post.id}. {post.title}</h2>
+                                <h4>By User ID {post.userId}</h4>
+                                <p>{post.body}</p>
+                                <hr />
+                            </div>
+                        ))
+                    ) : (
+                        this.state.error
+                        ? <p>{this.state.error}</p>
+                        : <h4>Loading posts ...</h4>
+                    )
+                }
             </div>
         )
     }
