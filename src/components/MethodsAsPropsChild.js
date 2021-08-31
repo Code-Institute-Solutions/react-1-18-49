0d3c00aa759f8b5da8d59600b012bad33ed20e61
mr-fibonacci <adamlapinski1@gmail.com>
2021-08-31 15:36:23 +0000

    023 finished challenge

--- src/components/MethodsAsPropsChild.js
+++ src/components/MethodsAsPropsChild.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function MethodsAsPropsChild(props) {
-    return (
+  return (
+    <div>
+      {props.isLoggedIn ? (
         <div>
-            {
-                props.isLoggedIn ? (
-                    <div>
-                        <p>Welcome to the site! Please complete the steps below:</p>
-                        <ol>
-                            <li>Confirm your email</li>
-                            <li>Complete your profile</li>
-                            <li>Subscribe to the newsletter</li>
-                        </ol>
-                    </div>
-                ) : (
-                    <p>Please sign in</p>
-                )
-            }
+          <p>Welcome to the site! Please complete the steps below:</p>
+          <ol>
+            <li>Confirm your email</li>
+            <li>Complete your profile</li>
+            <li>Subscribe to the newsletter</li>
+          </ol>
+          <button onClick={props.handleSignOut}>Sign out</button>
         </div>
-    )
+      ) : (
+        <div>
+          <p>Please sign in</p>
+          <button onClick={props.handleSignIn}>Sign in</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default MethodsAsPropsChild
+export default MethodsAsPropsChild;
--- src/components/MethodsAsPropsParent.js
+++ src/components/MethodsAsPropsParent.js
@@ -1,20 +1,46 @@
-import React, { Component } from 'react'
-import MethodsAsPropsChild from './MethodsAsPropsChild'
+import React, { Component } from "react";
+import MethodsAsPropsChild from "./MethodsAsPropsChild";
 
 class MethodsAsPropsParent extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isLoggedIn: true,
-        }
-    }
-    render() {
-        return (
-            <div>
-                <MethodsAsPropsChild isLoggedIn={this.state.isLoggedIn} />
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  handleSignIn = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+    console.log(this);
+  };
+
+  handleSignOut = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+    console.log(this);
+  };
+
+  // single method that handles both SignIn and SignOut
+  toggleIsLoggedIn = () => {
+    this.setState((prevState) => ({
+      isLoggedIn: !prevState.isLoggedIn,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <MethodsAsPropsChild
+          isLoggedIn={this.state.isLoggedIn}
+          handleSignIn={this.handleSignIn}
+          handleSignOut={this.handleSignOut}
+        />
+      </div>
+    );
+  }
 }
 
-export default MethodsAsPropsParent
+export default MethodsAsPropsParent;
