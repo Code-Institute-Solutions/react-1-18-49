1fd02fe3cfdd2e117138c6728b8cd8bcc76111d6
mr-fibonacci <adamlapinski1@gmail.com>
2021-08-31 14:53:04 +0000

    added commented out code

--- src/components/EventBinding.js
+++ src/components/EventBinding.js
@@ -1,34 +1,41 @@
 import React from "react";
 
 class EventBinding extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            introduction: "Hello!",
-            buttonText: "Exit",
-        };
-    }
-    
-    handleClick() {
-        this.setState({
-            introduction: "Goodbye!",
-        });
-        console.log(this.state.introduction);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      introduction: "Hello!",
+      buttonText: "Exit",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>
-                    {this.state.introduction} {this.props.greeting}
-                </h1>
-                <button onClick={() => this.handleClick()}>
-                    {this.state.buttonText}
-                </button>
-            </div>
-        )
-    }
+  //   handleClick() {
+  //     this.setState({
+  //       introduction: "Goodbye!",
+  //     });
+  //     console.log(this);
+  //   }
+
+  handleClick = () => {
+    this.setState({
+      introduction: "Goodbye!",
+    });
+    console.log(this);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.introduction} {this.props.greeting}
+        </h1>
+        {/* <button onClick={() => this.handleClick()}> */}
+        {/* <button onClick={this.handleClick.bind(this)}> */}
+        <button onClick={this.handleClick}>{this.state.buttonText}</button>
+      </div>
+    );
+  }
 }
 
 export default EventBinding;
