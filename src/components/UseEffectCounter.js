6034dd319564cc1ea0906bbf4a6073ed00820112
mr-fibonacci <adamlapinski1@gmail.com>
2021-09-01 10:26:35 +0000

    040 completed

--- src/components/UseEffectCounter.js
+++ src/components/UseEffectCounter.js
@@ -1,19 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function UseEffectCounter() {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        document.title = count;
-    });
-    return (
-        <div>
-            <button onClick={
-                () => setCount(count => count + 1)
-            }>
-                Increment Count ({count})
-            </button>
-        </div>
-    )
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("count 1 effect");
+    document.title = count;
+  }, [count]);
+  const [count2, setCount2] = useState(0);
+  useEffect(() => {
+    console.log("count 2 effect");
+    document.title = count2;
+  }, [count2]);
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        Increment Count ({count})
+      </button>
+      <button onClick={() => setCount2((count2) => count2 + 1)}>
+        Increment Count2 ({count2})
+      </button>
+    </div>
+  );
 }
 
-export default UseEffectCounter
+export default UseEffectCounter;
